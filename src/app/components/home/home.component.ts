f8cb4b9b8cc3ace3dce849ffa118c83ff8afcd03
Stefani Nikolova <stefani_nik@abv.bg>
2019-02-22 10:12:46 +0200

    Updated description for task 3

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,11 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  albumId: Number;
+
+  // TODO - 2: Define a variable which will store the value of the currently selected album id.
 
   constructor() { }
 
-  onShowAlbumImages(albumId: Number) {
-    this.albumId = albumId;
-  }
+  // TODO - 3: Implement a function which is subscribed to the event emitter from the Albums Table Component
+  // and accepts the emitted album id. This function is the one that is called from the template.
+  // In the function assign the accepted value to the defined variable.
 }
